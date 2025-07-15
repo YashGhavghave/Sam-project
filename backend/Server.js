bf19bface9cabb68a012e9b340eb51f7407487cd
YashGhavghave <yashghavghave123@gmail.com>
2025-07-15 13:24:38 +0530

    updated post req

--- backend/Server.js
+++ backend/Server.js
@@ -34,26 +34,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// ==== REGISTER ROUTE ====
-
-// app.post("/register", async (req, res) => {
-//   const { user, pass } = req.body;
-
-//   try {
-//     const existingUser = await DataModelSchema.findOne({ user });
-//     if (existingUser) {
-//       return res.status(409).json({ message: "User already exists" });
-//     }
-
-//     const newUser = await DataModelSchema.create({ user, pass });
-//     console.log("✅ User registered:", newUser.user);
-
-//     res.status(201).json({ message: "Registration Successful" });
-//   } catch (err) {
-//     console.error("❌ Registration error:", err);
-//     res.status(500).json({ message: "Registration Failed" });
-//   }
-// });
 
 app.use('/', userroute) 
 
