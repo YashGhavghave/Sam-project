b1a19325ee3f5be857338eaf9f3931f22cc0c836
YashGhavghave <yashghavghave123@gmail.com>
2025-09-03 00:12:10 +0530

    Yaml and folder structure

--- frontend/src/components/ConnectUs/ConnectPage.jsx
+++ frontend/src/components/ConnectUs/ConnectPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../LandingPage/PageComponents/Navbar'
-import Page from './PageComponents/page'
+import Page from './PageComponents/Page.jsx'
 import Footer from '../Footer/Footer'
 
 
